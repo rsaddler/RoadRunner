5617b26091d3c2a311e82dada06802dfdec7606e
rsaddler <radcliffesaddler@students.ptechnyc.org>
2015-05-10 02:23:40 -0400

    Some progression
    Some progression made. I am having trouble with making the hashkey. I
    cannot run the app.

---
+++ app/src/main/java/cst/roadrunner/MyApplication.java
@@ -0,0 +1,42 @@
+package cst.roadrunner;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Radcliffe Saddler
+ * I Used this file to help me setup my hashkey
+ * Created by admin on 5/10/2015.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey(){
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "cst.roadrunner",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("cst", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
